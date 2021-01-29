b36c1f2d6c0a602c4e02e03271af1dad5029271c
adarsha <adarsha@ucsb.edu>
2021-01-28 20:21:54 -0800

    ready to merge

--- myscript.js
+++ myscript.js
@@ -1,10 +1,9 @@
 
 function myClick(elem, src) {
-    
     event.preventDefault();
     console.log("HI");
     document.getElementById("modal").style.display = "block";
-    document.getElementById(src).src = src == "videoSrc" ? elem.src : elem.childNodes[0].src.toString();
+    document.getElementById(src).src = src == "videoSrc" ? elem.childNodes[1].src.toString() : elem.childNodes[0].src.toString();
 }
 
 function on(elem) {
@@ -13,4 +12,30 @@ function on(elem) {
 
 function off(elem) {
     document.getElementById("modal").style.display = "none";
+}
+
+function goTop(elem) {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+window.onscroll = function () {
+    if(document.body.scrollTop > 0.25* window.innerHeight || document.documentElement.scrollTop > 0.25*window.innerHeight) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+//using regex for email validatation purpose
+function validate(elem) {
+    document.getElementById("success").style.display = "none";
+    document.getElementById("failure").style.display = "none";
+
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.getElementById("email").value)) {
+        document.getElementById("success").style.display = "block";
+    } else {
+        document.getElementById("failure").style.display = "block";
+    }
 }
